8edefb6dd5bf6fc33b758e46604deed7dcec16af
OCY <zichengou@gmail.com>
2021-10-30 13:53:37 +0800

    s

--- src/jobs.ts
+++ src/jobs.ts
@@ -16,6 +16,7 @@ export function shedule(jobs: Job[], handler: (message: any, c: string) => void)
 			timmeout: 0,
 			worker: { workerData: { __filename: job.path } },
 			path: typescript_worker,
+			//神马情况
 			interval: `at ${job.start} every weekday`
 		};
 		buildJobs.push(w);
