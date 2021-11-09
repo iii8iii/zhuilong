8955157efbe02cfb46b617540a95d260dc1d704b
OCY <zichengou@gmail.com>
2021-11-09 12:06:51 +0800

    more zj and macdTrend in rule

--- src/jobs/JgetData.ts
+++ src/jobs/JgetData.ts
@@ -16,7 +16,7 @@ import { stockData } from "../types";
     });
   }
 
-  let result: stockData = { zt: [], zzt: [], qs: [], zj: [], wfzf: [] };
+  let result: stockData = { zt: [], zzt: [], qs: [], zj1: [], zj3: [], zj5: [], zj10: [], wfzf: [] };
   reRun(async () => {
     try {
       const zt = await getZtStocksInfo();
@@ -27,8 +27,18 @@ import { stockData } from "../types";
 
       const qs = await getQsStocksInfo();
       result.qs = qs.length ? delTp(tp, qs) : result.qs;
-      const zj = await getMoneyInStocks(3);
-      result.zj = zj.length ? delTp(tp, zj) : result.zj;
+
+      const zj1 = await getMoneyInStocks(1);
+      result.zj1 = zj1.length ? delTp(tp, zj1) : result.zj1;
+
+      const zj3 = await getMoneyInStocks(3);
+      result.zj3 = zj1.length ? delTp(tp, zj3) : result.zj3;
+
+      const zj5 = await getMoneyInStocks(5);
+      result.zj5 = zj1.length ? delTp(tp, zj5) : result.zj5;
+
+      const zj10 = await getMoneyInStocks(10);
+      result.zj10 = zj1.length ? delTp(tp, zj10) : result.zj10;
 
       const wfzf = await get5minZfStocks();
       result.wfzf = wfzf.length ? delTp(tp, wfzf) : result.wfzf;
--- src/jobs/Jnomal.ts
+++ src/jobs/Jnomal.ts
@@ -1,11 +1,13 @@
 import { parentPort, MessagePort } from "worker_threads";
 import { getStockCode, reRun } from "./utils";
-import { take, union } from 'lodash';
+import { difference, take, union, unionBy } from 'lodash';
 import { Result, stockData } from '../types';
+import { getKlineData } from '@iii8iii/dfcfbot';
+import { macdTrend } from '@iii8iii/analysts';
 
 (async () => {
   let ports: MessagePort[] = [];
-  let result: Result = { codes: [], zt: true };
+  let result: Result = { codes: [] };
   let codes: string[] = [];
 
   if (parentPort) {
@@ -16,7 +18,8 @@ import { Result, stockData } from '../types';
       }
       if (from) {
         from.on('message', async (data: stockData) => {
-          let { zj } = data;
+          let { zj1, zj3, zj5, zj10 } = data;
+          const zj = unionBy(zj3, zj5, zj1, zj10, 'c');
           codes = union(getStockCode(zj), codes);
         });
       }
@@ -24,7 +27,16 @@ import { Result, stockData } from '../types';
   }
 
   reRun(async () => {
-    result.codes = take(codes, 25);
+    codes = take(codes, 100);
+    for (const code of codes) {
+      const wData = await getKlineData(code, 'W');
+      const dData = await getKlineData(code, 'D');
+      if (wData && dData && macdTrend(wData) && macdTrend(dData)) {
+        result.codes = union([code], result.codes);
+      } else {
+        result.codes = difference(result.codes, [code]);
+      }
+    }
     for (const port of ports) {
       port.postMessage(result);
     }
