d59e570c71429f523012141175361dcd8864b7af
OCY <zichengou@gmail.com>
2021-11-17 15:33:12 +0800

    add macd to AN

--- src/jobs/Jafternoon.ts
+++ src/jobs/Jafternoon.ts
@@ -1,11 +1,14 @@
 import { parentPort, MessagePort } from "worker_threads";
 import { getStockCode, reRun } from "./utils";
 import { Result, stockData } from '../types';
+import { difference, take, union } from 'lodash';
+import { getKlineData } from '@iii8iii/dfcfbot';
+import { macdTrend } from '@iii8iii/analysts';
 
 (async () => {
   let ports: MessagePort[] = [];
   let result: Result = { codes: [] };
-
+  let codes: string[] = [];
   if (parentPort) {
     parentPort.on('message', (msg) => {
       const { to, from } = msg;
@@ -16,13 +19,24 @@ import { Result, stockData } from '../types';
         from.on('message', async (data: stockData) => {
           let { qs } = data;
           qs = qs.filter(item => item.zdp > 3 && item.nh);
-          result.codes = getStockCode(qs);
+          codes = union(getStockCode(qs), codes);
         });
       }
     });
   }
 
   reRun(async () => {
+    for (const code of codes) {
+      const dData = await getKlineData(code, 'D');
+      if (dData && macdTrend(dData)) {
+        result.codes = union([code], result.codes);
+      } else {
+        result.codes = difference(result.codes, [code]);
+      }
+    }
+
+    result.codes = take(result.codes, 50);
+
     for (const port of ports) {
       port.postMessage(result);
     }
