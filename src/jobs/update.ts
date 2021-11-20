dc9e0f1e15a722f7226bf89e01da78e84bb437d7
OCY <zichengou@gmail.com>
2021-11-20 16:49:59 +0800

    change update logic

--- src/jobs/update.ts
+++ src/jobs/update.ts
@@ -1,28 +1,35 @@
 import { parentPort } from "worker_threads";
-import { union } from "lodash";
+import { concat, difference, union, without } from "lodash";
 import { delZt } from './utils';
 import { Result } from "../types";
 
 (async () => {
 	let zx: string[] = [];
+	let toDels: string[] = [];
+	let dels: string[] = [];
 	if (parentPort) {
 		parentPort.on('message', msg => {
 			const { from } = msg;
 			from.on('message', async (result: Result) => {
 				let { zt, codes } = result;
+
 				if (!zt) {
 					codes = await delZt(codes);
 				}
 
-				console.log('length of codes from msg:', codes.length);
-				zx = union(codes, zx);
-				console.log('after union,zx length:', zx.length);
+				toDels = concat(without(toDels, ...codes), difference(zx, codes));
+				for (const toDel of toDels) {
+					if (toDels.filter(v => v === toDel).length >= 5) {
+						dels = union(dels, [toDel]);
+					}
+				}
+				toDels = without(toDels, ...dels);
+
+				zx = difference(union(codes, zx), dels);
 
 				if (parentPort) {
 					parentPort.postMessage(zx);
 				}
-				zx.pop();
-				console.log('after del,zx length:', zx.length);
 			});
 		});
 	}
