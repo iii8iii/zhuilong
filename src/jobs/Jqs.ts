d9279d62a1d0a0219cfd3ff61944d52a99fd0fc4
OCY <zichengou@gmail.com>
2021-11-29 14:59:02 +0800

    qs day macd highclose 3

--- src/jobs/Jqs.ts
+++ src/jobs/Jqs.ts
@@ -29,7 +29,7 @@ import { highClose, macdTrend } from '@iii8iii/analysts';
     for (const code of codes) {
       const dData = await getKlineData(code, 'D');
       const wData = await getKlineData(code, 'W');
-      if (dData && wData && macdTrend(wData) && macdTrend(dData) && highClose(dData)) {
+      if (dData && wData && macdTrend(wData) && macdTrend(dData, 'UP', 3) && highClose(dData, 3)) {
         result.codes = union([code], result.codes);
       } else {
         result.codes = difference(result.codes, [code]);
